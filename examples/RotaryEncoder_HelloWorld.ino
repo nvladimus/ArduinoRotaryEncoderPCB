da899a684cc493ffd27d66d162e12fe05d5bf6e8
Nikita Vladimirov <nvladimus@gmail.com>
2017-12-20 16:37:33 +0100

    + rotary encoder example

---
+++ examples/RotaryEncoder_HelloWorld.ino
@@ -0,0 +1,23 @@
+#include <Encoder.h> //encoder library https://www.pjrc.com/teensy/td_libs_Encoder.html
+
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(2,3);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Basic Encoder Test:");
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
